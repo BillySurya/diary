acc49336055927a94ea2f9113af07bba6748285e
Billy <billy.jakmall@gmail.com>
2021-05-22 15:35:20 +0700

    feat: use store for get list item

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    item: {},
+  },
+  getters: {
+    item: (state) => state.item,
+  },
+  mutations: {
+    setItemData(state, item) {
+      Vue.set(state.item, 'data', item);
+    },
+  },
+  actions: {
+    getItemList({ commit }) {
+      axios.get("http://localhost:3000/items").then((res) => {
+        commit("setItemData", res.data);
+      });
+    },
+  },
   modules: {},
 });
