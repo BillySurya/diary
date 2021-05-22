d064a9bcefbe4e1bdd5261701ab486db500f2b31
Billy <billy.jakmall@gmail.com>
2021-05-22 13:35:03 +0700

    feat: add new package and remove unused code

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+
+const moment = require('moment')
+require('moment/locale/id')
+
+Vue.use(require('vue-moment'), {
+  moment
+})
+
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
