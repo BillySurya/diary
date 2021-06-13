50cc0fb872e8e0c9f9a957f9efc258577b47c014
Billy <billy.jakmall@gmail.com>
2021-06-13 09:19:54 +0700

    feat: implement tailwind class on diary Modal

--- src/components/DiaryModal.vue
+++ src/components/DiaryModal.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-    <div>
-      <header>
-        <slot name="header">
-          <div>
-            Tambah Entri
-          </div>
-        </slot>
+  <div class="fixed inset-0 bg-gray-900 bg-opacity-60 flex justify-center items-center">
+    <div class="bg-white overscroll-x-auto flex flex-col w-72 rounded-sm p-3">
+      <header class="p-2 flex relative border-b font-medium text-md justify-between items-center">
+        <div>
+          Tambah Entri
+        </div>
         <div>
           <button
             type="button"
@@ -17,28 +15,28 @@
         </div>
       </header>
 
-      <section>
-        <slot name="body">
-          <div>
-            <label for="name">Nama</label>
-            <input name="name" type="text" v-model="diary.name">
-          </div>
-          <div>
-            <label for="cost">Harga</label>
-            <input name="cost" type="number" v-model="diary.cost">
-          </div>
-        </slot>
-       </section>
+      <section class="relative py-3 px-2">
+        <div class="flex flex-col mb-3">
+          <label class="text-sm" for="name">Nama</label>
+          <input class="border border-black rounded-md p-0.5 px-2" name="name" type="text" v-model="diary.name">
+        </div>
+        <div class="flex flex-col mb-3">
+          <label class="text-sm" for="cost">Harga</label>
+          <input class="border border-black rounded-md p-0.5 px-2" name="cost" type="number" v-model="diary.cost">
+        </div>
+      </section>
 
-      <footer>
+      <footer class="p-2 flex border-t justify-end">
         <button
           type="button"
+          class="text-black text-sm rounded-md py-2 px-3 cursor-pointer border-none bg-gray-200 mr-2"
           @click="close"
         >
           Batal
         </button>
-         <button
+        <button
           type="button"
+          class="text-white text-sm rounded-md py-2 px-3 cursor-pointer border-none bg-purple-900"
           @click="submit"
         >
           Kirim
