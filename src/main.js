f8ad828e1dfeede0867134e2f9557c449b976959
Billy <billy.jakmall@gmail.com>
2021-06-13 09:01:13 +0700

    feat: install package tailwind css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+import "tailwindcss/tailwind.css"
+
 const moment = require('moment')
 require('moment/locale/id')
 
